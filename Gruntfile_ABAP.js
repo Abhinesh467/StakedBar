5f43070d7d75506a112fe43f904bc6e570e0d25d
Abhinesh467 <60090214+Abhinesh467@users.noreply.github.com>
2020-02-19 21:43:41 +0530

    Update Gruntfile_ABAP.js

--- Gruntfile_ABAP.js
+++ Gruntfile_ABAP.js
@@ -24,7 +24,7 @@
 
 					var abapPackage = "ZFIORI";
 
-					var transportRequest = "ND5K900200";
+					var transportRequest = "  ND5K900202";
 
 					var jobURL = process.env.JOB_URL;
 
@@ -271,7 +271,7 @@
 
 								//             transportRequest = JSON.parse(fs.readFileSync(ctsDataFile, { encoding: "utf8" })).REQUESTID;
 
-								transportRequest = "ND5K900200";
+								transportRequest = "  ND5K900202";
 
 							}
 
